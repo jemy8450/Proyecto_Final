564605b1275056f0e43e72e07e1d17e7b3cddb7e
segur <segur@JEREMYGAMER8450>
2024-04-10 21:09:47 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidades/Venta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Venta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VentasId { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public decimal Total { get; set; }
+
+        // propiedades de navegacion
+        public List<DetallesdelaVenta> Detalles { get; set; }
+    }
+}
